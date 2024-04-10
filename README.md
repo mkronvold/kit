32da167a8ad66f291940414b264586836f4b2c30
mkronvold <mkronvold@dev2013.dev.e2open.com>
2024-04-10 21:22:16 +0000

    initial re-commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# kit
-kubernetes inventory html generator for tmc
+# Kubernetes Inventory Tools
+
+# Description:
+The tool allows you to list the resources available on a Kubernetes cluster and to make a static report in the form of an HTML file.
+
+The account when running the script is taken from the .kube / config file.
+
+# How works?
+
+The account used must have read access (list) on the following resources:
+- nodes
+- namespaces
+- secrets
+- configmaps
+- deployment
+- pods
+- pvcs
+
---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#figure out where we are
+
+[ $(hostname -s) == "dev2013" ] && site=sv4 && env=dev
+
+[ $(hostname -s) == "stg555" ] && site=sv4 && env=stg
+[ $(hostname -s) == "stgf1088" ] && site=fr8 && env=stg
+
+[ $(hostname -s) == "prod10506" ] && site=sv1 && env=prod
+[ $(hostname -s) == "prod41773" ] && site=ch3 && env=prod
+[ $(hostname -s) == "prod3029" ] && site=sha && env=prod
+[ $(hostname -s) == "prod9088" ] && site=fr8 && env=prod
+
+[ $(hostname -s) == "prod8073" ] && site=cdg && env=dr
+[ $(hostname -s) == "prod7012" ] && site=de2 && env=dr
+
+commonname=$site-$env
+
+# and what our WCP is
+
+supervisor=$(tanzu tmc management-cluster list -o json | jq -r '.managementClusters[] | "\(.meta.labels.cn),\(.fullName.name)"' | csvgrep -H -c 1 -m ${commonname} | csvcut -c 2 | tail -n +2)
+
+# from the WCP cluster list, change context to that cluster
+# run the kit
+# and rename the output file
+
+for cluster in $(tanzu tmc cluster list -m $supervisor -o json | jq -r '.clusters[] | "\(.fullName.name)"')
+do
+  k ctx $cluster
+  go run .
+  mv cluster.html $cluster.html
+done
+
+ls -1 *.html
