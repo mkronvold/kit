61ec1ed96100ef710f7c639bcf9e55748a81c25d
mkronvold <mkronvold@dev2013.dev.e2open.com>
2024-04-11 15:47:56 +0000

    once an hour

--- kit.cron.d
+++ kit.cron.d
@@ -1,4 +1,4 @@
 ## Adjust this line...
 MAILTO=mkronvold
 
-0  */3 * * * mkronvold /e2open/home/mkronvold/src/kit/push.sh > /dev/null
+0 * * * * mkronvold /e2open/home/mkronvold/src/kit/push.sh > /dev/null
