63a5045718bbd224703bae96f74e3dc5e98a5f55
mkronvold <mkronvold@dev2013.dev.e2open.com>
2024-04-11 16:35:38 +0000

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go build . && cp kubernetes-inventory ../e2kpe.github.io && echo Copied new binary to e2kpe.github.io
+go build . && cp kubernetes-inventory ../e2kpe.github.io && echo Copied new binary to e2kpe.github.io && pushd ../e2kpe.github.io && git add kubernetes-inventory && git commit -m "updated kit" && git push && popd
